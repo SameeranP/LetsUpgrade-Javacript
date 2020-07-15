59a1af2bac604279319605800086b47d4f760d6e
SameeranP <51751179+SameeranP@users.noreply.github.com>
2020-07-15 13:19:37 +0530

    Difference between var, const and let
    2nd assignment

---
+++ Day1/Assignment_2_day1.js
@@ -0,0 +1,26 @@
+console.log("Hello again")
+//variables in js
+var name = "Sameeran";
+console.log(name)
+var name = "Not sameeran anymore"
+console.log(name)
+
+const id_num = 11810951;
+console.log(id_num);
+//id_num = 1210091;
+//console.log(id_num);
+
+const fruits = ["apple","banana","grapes"]
+console.log(fruits)
+fruits.push("orange")
+console.log(fruits)
+//fruits = ["apple", "banana", "chikoo"]
+
+var city = "Delhi"
+
+{
+let city = "Nagpur"
+console.log(city)
+
+}
+console.log(city)
