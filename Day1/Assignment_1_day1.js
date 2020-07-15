eb2847d744e1030a721b2068f48835eecb579a24
SameeranP <51751179+SameeranP@users.noreply.github.com>
2020-07-15 12:37:23 +0530

    Assignment 1 day 1

---
+++ Day1/Assignment_1_day1.js
@@ -0,0 +1,10 @@
+console.time("your code took:")// Initialize timing the codeblock
+console.log('Hello this is your console') //This is used to write something in the console
+console.table({name : "sam", age: "20", marks: "100"  }) //Used to view data in tabular form
+console.log({name : "sam", age: "20", marks: "100"  }) 
+console.warn("This is a warning from the user. I am warning you!!") //Used to give a warning
+
+console.timeEnd("your code took:")// display the time it took to run this code block
+var age=190
+console.assert(age<100, 'Age >100 not possible') //Used to assert if the given statement is true or not
+console.error("This is an error but don't take it seriously") //To give user an error message
